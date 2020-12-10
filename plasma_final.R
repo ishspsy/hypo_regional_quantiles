5b030d2dcfd8576eb21076f7d92dc88f7aa14837
ishspsy <ishspsy@gmail.com>
2020-12-10 14:43:40 +0900

    Update plasma_final.R

--- plasma_final.R
+++ plasma_final.R
@@ -1,6 +1,6 @@
 #### Real Boston data 
 
-data_real_ind = "boston"
+data_real_ind = "plasma"
 
 
 # Set "plot_save = 1" for generating coefficient functions
@@ -35,50 +35,43 @@ if (taus == "high"){
 }
 
 
+# load Plasma data
+plasma = read.csv("doc7.txt", sep= "") ;  
 
+# Take log transformation of the raw data
+Y = plasma[,13]
+Y = log(Y+1)
+age = plasma[,1]
+smoke = plasma[,3]
+BMI = plasma[,4]
+vit = plasma[,5]    
+calo =  plasma[,6]
+fat =  plasma[,7]
+fiber =  plasma[,8]  
+alcoh = plasma[,9]
+chole = plasma[,10]
+U = plasma[,11]
 
-# In the testing setting, must set to be "ttest != 0"
-if (ttest ==1){
-ttype = "tau"   # (independent of both)
-type="known"}
-
-if (ttest ==2){
-  ttype = "tau"   # (independent of t)
-  type="unknown"}
-
-
-# load Boston data
-boston = read.csv("Boston_data.csv", sep= "")  
-
-Y = boston[,14]
-U = boston[,13]
-Y= Y-mean(Y)
-
-# Choose the varaibles
-X =   boston[,c(6,1,10,11,5,7)]  
-U = (U - min(U)) / (max(U) - min(U))
-
-n = dim(X)[1] 
+coef_names = c("Intercept","Smoking status (never)","Smoking status(former)", "Vitamin (fairly often)","Vitamin (not often)", "Age",
+               "BMI", "Calories","Fat","Fiber","Alcohol","Cholesterol")
+X =cbind(smoke==1,smoke==2, vit==1,vit==2,age,BMI,calo,fat,fiber,alcoh,chole)
+n = dim(X)[1]
 p = dim(X)[2]
-
 X = cbind(X, rep(1, n))
 
 n = dim(X)[1]
 p = dim(X)[2]
+p = p-1
 
 # Normalize data
-X[,1:(p-1)] = X[,1:(p-1)] -  matrix(rep(apply(X[,1:(p-1)],2,mean),n), nrow =n, byrow=T) 
-
-yy = sqrt(apply(X[,1:(p-1)]^2,2,mean))
-
+X[,5:p] = X[,5:p] -  matrix(rep(apply(X[,5:p],2,mean),n), nrow =n, byrow=T)    
+yy = sqrt(apply(X[,5:p]^2,2,mean))
 # intercept, sex, smoking, height, height * smoking
-X = cbind(X[,1:(p-1)] /  matrix(rep(yy,n), nrow=n,byrow=T), rep(1, n)); 
+X = cbind(X[,1:4], X[,5:p] /  matrix(rep(yy,n), nrow=n,byrow=T), rep(1, n)); 
 p = dim(X)[2]
-U = (U - min(U)) / (max(U) - min(U))
+U = (U - min(U)) / (max(U) - min(U)); 
 t_point = U
 
-
-## Start
 # X_tr has an intercept in the last column
 X_tr <- X
 Y = sqrt(length(Y)) * Y / sqrt(sum(Y^2))
@@ -87,46 +80,45 @@ Y_tr <- Y
 ## Dimension
 n = dim(X_tr)[1]
 p = dim(X_tr)[2]
-pp = p
-
-
 ind0 <- 1:dim(X_tr)[2]
-coef_names = c("Intercept","Rm","Crime","Tax","Ptratio","Nox","Age")
-#1   6    % rm average number of rooms per dwelling
-#2   1 % per capita crime rate by town (CRIM),
-#3   10  % tax	full-value property-tax rate per USD 10,000
-#4   11 % ptratio	pupil-teacher ratio by town
-#5    5  % nitric oxides concentration(parts per 10 million, NOX),
-#6    7  % age	proportion of owner-occupied units built prior to 1940
+
 
 X_tr <- cbind(X_tr[,p], X_tr[,1:(p-1)])
 
-# Choose variables of interest (In the estimation setting, choose all variables)
-if (ttest ==0){ind00_origin = 1:7}
-if (ttest == 1){ind00_origin = c(4,7)}  # test1: tax and age
-if (ttest == 2){ind00_origin = 2}  # test2:rm
+## In the estimation procedure, must set "ttest == 0"
+if (ttest ==0){ind00_origin = 1:12}
+if (ttest==1){ind00_origin = c(6,7,8,9,11,12)}  # test1:  age bmi cal fat alc chol
+if (ttest==2){ind00_origin = c(2,3,4,5,10)}  # test2:smoking whether independent w.r.t. tau
+if (ttest==3){ind00_origin = c(2,3)}  # test3:smoking parallel
+if (ttest==4){ind00_origin = c(4,5)}  # test3:vitamin parallel
 
-ind00 = ind00_origin
+ind00=ind00_origin
 
-# Change of variables such that the last few variables are considered in the null hypothesis.
+# Transform data such that the last few variables are related to the null hypothesis
 X_tr = cbind(X_tr[, -ind00],   X_tr[,ind00])
 coef_names_updated = c(coef_names[-ind00], coef_names[ind00])
 
-if (ttest ==0){ind00 = 1:7}
-if (ttest == 1){ind00 = c(6,7)}  # test1: tax and age
-if (ttest == 2){ind00 = c(7)}  # test2:rm
+if (ttest==3 | ttest==4){
+X_tr = cbind(X_tr[,1:10],X_tr[,11]+X_tr[,12], X_tr[,12])
+coef_names_updated = c(coef_names[-ind00], coef_names[ind00])
+} #only for test3:smoking parallel
+
+if (ttest==1){ind00 = 7:12}   # test1:  age bmi cal fat alc chol
+if (ttest==2){ind00 = 8:12}  # test2:smoking
+if (ttest==3 | ttest==4){ind00 = 12}  # test3:smoking
+
 
 # Estimating for lower quantile region
 data4_real_coef= real_main_function(taus="low", n,p,X_tr, Y_tr,t_point)
-save(data4_real_coef, file="data4_real_boston_coef_low.RData")
+save(data4_real_coef, file="data4_real_plasma_coef_low.RData")
 
 # Estimating for middle quantile region
 data4_real_coef= real_main_function(taus="middle", n,p,X_tr, Y_tr,t_point)
-save(data4_real_coef, file="data4_real_boston_coef_middle.RData")
+save(data4_real_coef, file="data4_real_plasma_coef_middle.RData")
 
 # Estimating for upper quantile region
 data4_real_coef= real_main_function(taus="high", n,p,X_tr, Y_tr,t_point)
-save(data4_real_coef, file="data4_real_boston_coef_high.RData")
+save(data4_real_coef, file="data4_real_plasma_coef_high.RData")
 
 coef_mat_set = data4_real_coef$coef_mat_set
 
